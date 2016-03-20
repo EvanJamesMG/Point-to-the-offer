68e9e26e5ff965ad4b37278f33d39fcca594e492
EMG <Evan123mg@gmail.com>
2016-03-20 12:33:17 +0800

    Update 二叉搜索树的后序遍历序列.py

--- python/二叉搜索树的后序遍历序列.py
+++ python/二叉搜索树的后序遍历序列.py
@@ -51,31 +51,51 @@ class Solution:
 java版本
 
 public class test {
-	
-	
+
 	public static void main(String[] args){
 		
-		int [] sequence = {5,7,6,9,11,10,8};
-		
-		VerifySquenceOfBST(sequence);
+		int [] sequence ={5,7,6,9,11,10,8};
+		boolean res = VerifySquenceOfBST(sequence);
+        	System.out.print(res);
 	}
 
     public static boolean VerifySquenceOfBST(int [] sequence) {
-    	return isBST(sequence, 0, sequence.length-1);
-    }
-    public static boolean isBST(int[] array, int begin, int end) {
-        int left = begin;
-        int right = end - 1;
-        if(end - begin <= 1)
-            return true;
-        while (left < end && array[end] > array[left])
-            left++;
-        while (right > begin && array[end] < array[right])
-            right--;
-        if(left - 1 == right || left == right)
-            return isBST(array, begin, left - 1) && isBST(array, left, end - 1);
-        else
-            return false;
+        int root;
+    	int breakindex=0;
+  
+    	if(sequence == null || sequence.length==0){
+    		return false;
+    	}
+      	int length = sequence.length;
+    	root = sequence[length-1];
+    	while(breakindex<length-1 && sequence[breakindex]<root){
+    		breakindex++;
+    	}
+    	for(int i=breakindex;i<length-1;i++){
+    		if(sequence[i]<root)
+    			return false;
+    	}
+    	
+    	boolean left = true;
+
+    	if(breakindex>0){
+        	int[] leftsequence = new int[breakindex] ;
+        	for(int i=0;i<breakindex;i++){
+        		leftsequence[i]=sequence[i];
+        	}
+    		left = VerifySquenceOfBST(leftsequence);
+    	}
+    	
+    	boolean right = true;
+
+    	if(breakindex<length-1){    
+        	int[] rightsequence = new int[length-1-breakindex];
+	    	for(int i=breakindex;i<length-1;i++){
+	    		rightsequence[i-breakindex]=sequence[i];
+	    	}
+    		right = VerifySquenceOfBST(rightsequence);
+    	}
+    	return left && right;
     }
 }
 '''
